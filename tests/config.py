8dbe1c2125a47f91aef01e1b583f684e31ccd368
Mangiafesto Electronics LLC <ZacharyMangiafesto@MangiafestoElectronics.onmicrosoft.com>
2025-12-04 14:24:24 -0500

    Create config.py
    Test V1

---
+++ tests/config.py
@@ -0,0 +1,12 @@
+REFRESH_SECONDS_DEFAULT = 30
+DEFAULT_QUOTE = "USDT"
+PROFIT_PCT_DEFAULT = 300.0
+
+# Storage location in user home
+APP_DIRNAME = ".chainguardian"
+STORE_FILENAME = "store.enc"
+KEY_FILENAME = "fernet.key"
+
+# Market data
+COINGECKO_TIMEOUT = 6
+FEAR_GREED_TIMEOUT = 6
