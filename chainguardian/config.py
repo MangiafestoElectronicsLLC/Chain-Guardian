de0f95466a5b7bef05b7fd17d49c4d4fa347ddab
Mangiafesto Electronics LLC <ZacharyMangiafesto@MangiafestoElectronics.onmicrosoft.com>
2025-12-08 16:55:04 -0500

    Create config.py
    Stream lit V2

---
+++ chainguardian/config.py
@@ -0,0 +1,9 @@
+import os
+
+APP_DIRNAME = ".chainguardian"
+KEY_FILENAME = "fernet.key"
+STORE_FILENAME = "store.enc"
+
+REFRESH_SECONDS_DEFAULT = 60
+DEFAULT_QUOTE = "USD"
+PROFIT_PCT_DEFAULT = 300.0  # default profit-take threshold
